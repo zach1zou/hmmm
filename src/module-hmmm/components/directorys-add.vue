5529c37c203fb243905ae9cc6e399ee7b6f620c7
SweetGirl <1762088644@qq.com>
2022-08-19 22:01:14 +0800

    弹框组件

--- src/module-hmmm/components/directorys-add.vue
+++ src/module-hmmm/components/directorys-add.vue
@@ -1,9 +1,103 @@
 <template>
-  <div class='container'>添加目录对话框</div>
+  <div class="container">
+    <!-- 添加目录弹框 -->
+    <el-dialog
+      title="新增目录"
+      :visible="addDialogVisible"
+      width="20%"
+      @close="restDir"
+    >
+      <!-- 主体区域 -->
+      <el-row>
+        <el-col>
+          <el-form
+            :model="directorysForm"
+            :rules="directorysFormRules"
+            ref="directorysFormRef"
+            label-width="80px"
+            class="demo-ruleForm"
+          >
+            <el-form-item
+              v-if="!subjects.id && !subjects.name"
+              class="elform"
+              label="所属学科"
+            >
+              <el-select
+                v-model="directorysForm.value"
+                placeholder="请选择"
+                style="width: 100%"
+              >
+                <el-option
+                  :label="item.label"
+                  :value="item.value"
+                  v-for="item in addFrom"
+                  :key="item.id"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item class="elform" label="目录名称" prop="directoryName">
+              <el-input
+                v-model="directorysForm.directoryName"
+                placeholder="请输入目录名称"
+                style="width: 100%"
+              ></el-input>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addDirForm">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      addDialogVisible: false,
+      // 验证
+      directorysFormRules: {
+        directoryName: [
+          { required: true, message: "请输入目录名称", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "blur" },
+        ],
+      },
+      // 添加目录的数据
+      directorysForm: {
+        directoryName: "",
+        // 下拉框的值
+        value: "",
+      },
+    };
+  },
+  props: {
+    subjects: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    restDir() {
+      this.$emit("restDir");
+    },
+    addDirForm() {
+      this.addDialogVisible = false;
+      this.$emit("addDirForm");
+    },
+  },
+};
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang="less">
+.elform {
+  width: 100%;
+}
+
+.el-form-item__content {
+  margin-left: 10px;
+}
+</style>
--- src/module-hmmm/components/subjects-add.vue
+++ src/module-hmmm/components/subjects-add.vue
@@ -1,9 +1,85 @@
 <template>
-  <div class='container'>添加学科对话框</div>
+  <div class="container">
+    <!-- 添加的弹框 -->
+    <el-dialog
+      title="新增学科"
+      :visible="addDialogVisible"
+      width="20%"
+      @close="resetSub"
+    >
+      <!-- 主体区域 -->
+      <el-row>
+        <el-col>
+          <el-form
+            :model="addSubjectsFrom"
+            :rules="addSubjectsFromRules"
+            ref="addSubjectsFromRef"
+            label-width="80px"
+            class="demo-ruleForm"
+          >
+            <el-form-item label="学科名称" prop="subjectName">
+              <el-input
+                v-model="addSubjectsFrom.subjectName"
+                placeholder="请输入学科名称"
+              ></el-input>
+            </el-form-item>
+          </el-form>
+        </el-col>
+        <span class="subspan">是否显示</span>
+        <el-switch
+          v-model="SubSwitchValue"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        >
+        </el-switch>
+      </el-row>
+      <!-- 按钮 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addSubFrom">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      addDialogVisible: false,
+      // 添加学科是否显示的开关
+      SubSwitchValue: true,
+      // 添加的验证
+      addSubjectsFromRules: {
+        subjectName: [
+          { required: true, message: "请输入学科名称", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "blur" },
+        ],
+      },
+      // 添加的输入框数据
+      addSubjectsFrom: {
+        subjectName: "",
+      },
+    };
+  },
+  methods: {
+    resetSub() {
+      this.$emit("resetSub");
+    },
+    addSubFrom() {
+      this.addDialogVisible = false;
+      this.$emit("addSubFrom");
+    },
+  },
+};
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang="less">
+.elform {
+  width: 100%;
+}
+
+.el-form-item__content {
+  margin-left: 10px;
+}
+</style>
