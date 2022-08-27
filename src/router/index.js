2818e4ce4ac9a33bd387c406538bd2dee2043fc3
songhao <1146987523@qq.com>
2022-08-27 10:42:58 +0800

    22222

--- src/router/index.js
+++ src/router/index.js
@@ -111,6 +111,12 @@ router.beforeEach((to, from, next) => {
   }
 })
 
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalPush.call(this, location, onResolve, onReject);
+  return originalPush.call(this, location).catch((err) => err);
+};
 router.afterEach(() => {
   NProgress.done() // finish progress bar
 })
